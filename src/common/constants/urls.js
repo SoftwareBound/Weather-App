c69ea38cb537eb585b214a27b060c0125a40e949
unknown <benzvi.ron@gmail.com>
2021-04-29 13:10:54 +0300

    fix app try#5

--- src/common/constants/urls.js
+++ src/common/constants/urls.js
@@ -6,13 +6,13 @@ export const iconUrls = {
 };
 
 export const weatherUrls = {
-  CITY_CURRENT_WEATHER: ` http://dataservice.accuweather.com/currentconditions/v1/`,
+  CITY_CURRENT_WEATHER: ` https://dataservice.accuweather.com/currentconditions/v1/`,
   CITY_FIVE_DAY_FORECAST:
-    "http://dataservice.accuweather.com/forecasts/v1/daily/5day/",
+    "https://dataservice.accuweather.com/forecasts/v1/daily/5day/",
 };
 export const apiKeyUrls = {
   APIKEY_URL: `?apikey=${apiKeys.WEATHER_APIKEY}`,
 };
 export const searchUrls = {
-  AUTO_COMPLETE_CITY_SEARCH: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
+  AUTO_COMPLETE_CITY_SEARCH: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
 };
