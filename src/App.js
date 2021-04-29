b34b787f2acb8474f4990612250a9c0d26b8a27a
unknown <benzvi.ron@gmail.com>
2021-04-29 12:17:43 +0300

    fix app try#2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path="/">
+              <Route exact path="/ron-benzvi-25-04-21">
                 <div
                   className="row search"
                   style={{
